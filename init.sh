fd719a5cf21e12ff7db229085315ef16811a7cfd
Ryan <ryan.eliopoulos@gmail.com>
2022-11-22 15:32:55 -0800

    +deploy stuff

--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@ pipenv run flask --app sale_monitor init-db
 
 #  This is the work around to python import system being total garbage.
 ln ./sale_monitor/kroger/Communicator.py ./sale_monitor/checker/Communicator.py  # Sym link doesn't work.
+cd ./sale_monitor/checker  # need this to avoid collateral damage
 grep -rl 'Communicator' . | xargs sed -i 's%sale_monitor\.kroger\.Communicator%Communicator%'  #
